c3619ff7c0a1c08da21bbd7a78044ce8d6ec46f1
Tharindu Sampath Balasooriya <tharindutsb@gmail.com>
2025-02-02 16:02:05 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from openApi.routes import student_routes
+from openApi.routes import intern_routes
 
 app = FastAPI()
 
-# Include the student routes
-app.include_router(student_routes.router)
+# Include the intern routes
+app.include_router(intern_routes.router)
